4305e95270570aa006c2b722de90217a834ecffe
HengyuanZhu <2835341515@qq.com>
2026-04-30 22:44:59 +0800

    第二版本代码，实现将用户键盘输入的内容显示输出，注意事项见README.md

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello,World!\n");
+    char input[100];
+    printf("请从键盘输入任意内容，不超过100个字符！\n");
+    fgets(input,101,stdin);
+    printf("你输出的内容为：\n%s",input);
     return 0;
 }
